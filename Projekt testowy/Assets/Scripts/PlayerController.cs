fd6b37d7c89c6c417f45b72157fe6427f9a899de
dsladeck <hertores@wp.pl>
2024-10-27 15:30:34 +0100

    TESTZ

--- Projekt testowy/Assets/Scripts/PlayerController.cs
+++ Projekt testowy/Assets/Scripts/PlayerController.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    int liczbacalkowita;
-    float liczbaprzecinkowa = 0.1f;
+
+    int liczbacalkowita; // np. 1,2,54,76
+    float liczbaprzecinkowa = 0.1f; // np, 4.32423, 56.23423
 
     public float speed = 5;
+
     public Rigidbody rigidbody;
+
+    public Transform startPoint;
+
+    public int zebranePunkty;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
-        //Wszystko co wpiszemy tutak wykonuje sie pod czas startu
         rigidbody = GetComponent<Rigidbody>();
+
+
+        //Wszystko co tutaj zamiescimy wykonuje sie podczas staru gry/aplikacji
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetAxis("Horizontal") != 0)
         {
             float directionHorizontal = Input.GetAxis("Horizontal");
@@ -33,6 +43,14 @@ public class PlayerController : MonoBehaviour
             float directionVertical = Input.GetAxis("Vertical");
             rigidbody.AddForce(0, 0, directionVertical * Time.deltaTime * speed, ForceMode.Impulse);
         }
+
+        if (transform.position.y < 0)
+        {
+            transform.position = startPoint.position;
+            rigidbody.velocity = Vector3.zero;
+        }
+
+
 
 
     }
